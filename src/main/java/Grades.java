f707c80bfcd5be610b090a6a65e2a23e9cfa7926
Win10 <emese0245@gmail.com>
2022-04-01 09:46:46 +0200

    x

---
+++ src/main/java/Grades.java
@@ -0,0 +1,13 @@
+public class Grades {
+    public static void main(String[] args){
+        Grades grades = new Grades();
+        Subject subject = new Subject();
+        Subject subjectGrades = new Grades();
+        grades.setName("3");
+        subject.setName("Programozás");
+
+
+
+    }
+
+}
---
+++ src/main/java/Matrix.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Matrix {
+    public static void sumOfEvenNumbers(int[][] matrix){
+
+    }
+
+}
